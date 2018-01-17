52d9b8a235bbcbb2edec809ecd7944ad9930169a
shikuigang2 <278140970@qq.com>
2018-01-17 16:41:37 +0800

    11

--- m-api/src/main/java/com/ztth/api/path/rest/ApiPathController.java
+++ m-api/src/main/java/com/ztth/api/path/rest/ApiPathController.java
@@ -3,11 +3,16 @@ package com.ztth.api.path.rest;
 import com.ztth.api.path.biz.ApiPathBiz;
 import com.ztth.api.path.biz.RedisQueueBiz;
 import com.ztth.api.path.config.MessageConfig;
+import com.ztth.core.constant.ServerConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api")
@@ -34,18 +39,83 @@ public class ApiPathController {
 
     @RequestMapping(value = "/waitingMSM")
     @ResponseBody
-    public ResponseEntity<?> getWaitSMS() throws Exception {
+    public ResponseEntity<?> getWaitSMS( String channel) throws Exception {
 
-        return ResponseEntity.status(200).body(redisQueueBiz.getQueueList(messageConfig.getQueueIn()));
+        if(channel != null){
+            return ResponseEntity.status(200).body(redisQueueBiz.getQueueList(ServerConstant.QUEUE_PREFIX+channel).size());
+        }else{
+            Long  totalLength = redisQueueBiz.getTotalLength(ServerConstant.WAITING_SET);
+            Map<String,Long> result = new HashMap<String,Long>();
+            result.put("totalLen",totalLength);
+            return ResponseEntity.status(200).body(result);
+        }
     }
+
+    @RequestMapping(value = "/waitingMSMData")
+    @ResponseBody
+    public ResponseEntity<?> waitingMSMData( String channel) throws Exception {
+
+        if(channel != null){
+            return ResponseEntity.status(200).body(redisQueueBiz.getQueueList(ServerConstant.QUEUE_PREFIX+channel));
+        }else{
+            Long  totalLength = redisQueueBiz.getTotalLength(ServerConstant.WAITING_SET);
+            Map<String,Long> result = new HashMap<String,Long>();
+            result.put("totalLen",totalLength);
+            return ResponseEntity.status(200).body(result);
+        }
+    }
+
 
     /**
      * 获取当前 并发中的 信息数量
      */
     @RequestMapping(value = "/sendingMSM")
     @ResponseBody
-    public ResponseEntity<?> getSendingSMS() throws Exception {
-        return ResponseEntity.status(200).body(redisQueueBiz.getQueueList(messageConfig.getQueueOut()));
+    public ResponseEntity<?> getSendingSMS(String channel) throws Exception {//@RequestParam
+        //通过渠道号 获取当前渠道的并发数
+        if(channel != null){
+            return ResponseEntity.status(200).body(redisQueueBiz.getQueueList(ServerConstant.SEND_PREFIX+channel).size());
+        }else{
+            Long  totalLength = redisQueueBiz.getTotalLength(ServerConstant.SENDING_SET);
+            Map<String,Long> result = new HashMap<String,Long>();
+            result.put("totalLen",totalLength);
+            return ResponseEntity.status(200).body(result);
+        }
+    }
+
+    /**
+     * 获取当前 并发中的 信息数量
+     */
+    @RequestMapping(value = "/sendingMSMData")
+    @ResponseBody
+    public ResponseEntity<?> sendingMSMData(String channel) throws Exception {//@RequestParam
+        //通过渠道号 获取当前渠道的并发数
+        if(channel != null){
+            return ResponseEntity.status(200).body(redisQueueBiz.getQueueList(ServerConstant.SEND_PREFIX+channel));
+        }else{
+            Long  totalLength = redisQueueBiz.getTotalLength(ServerConstant.SENDING_SET);
+            Map<String,Long> result = new HashMap<String,Long>();
+            result.put("totalLen",totalLength);
+            return ResponseEntity.status(200).body(result);
+        }
+    }
+
+
+    /**
+     * 通过 渠道 编号获取当前并发数据
+     */
+    @RequestMapping(value = "/sending")
+    @ResponseBody
+    public ResponseEntity<?> getSendingSMSByChannel(@RequestParam String channel) throws Exception {
+        //return ResponseEntity.status(200).body(redisQueueBiz.getQueueList(messageConfig.getQueueOut()));
+        return null;
+    }
+
+    @RequestMapping(value = "/msmCount")
+    @ResponseBody
+    public ResponseEntity<?> msmCount(@RequestParam String channel) throws Exception {
+        //return ResponseEntity.status(200).body(redisQueueBiz.getQueueList(messageConfig.getQueueOut()));
+        return null;
     }
 
 }
--- m-api/src/main/java/com/ztth/api/path/rest/msgSendController.java
+++ m-api/src/main/java/com/ztth/api/path/rest/msgSendController.java
@@ -1,13 +1,19 @@
 package com.ztth.api.path.rest;
 
 import com.alibaba.fastjson.JSON;
+import com.ztth.api.path.biz.MobileChannelBiz;
+import com.ztth.api.path.biz.MobileDataBiz;
 import com.ztth.api.path.biz.MsgLogBiz;
 import com.ztth.api.path.biz.RedisQueueBiz;
 import com.ztth.api.path.config.MessageConfig;
 import com.ztth.api.path.entity.Message;
+import com.ztth.api.path.entity.MobileData;
+import com.ztth.core.constant.ServerConstant;
+import com.ztth.core.util.PhoneFormatCheckUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +21,6 @@ import java.util.Date;
 
 @RestController
 public class msgSendController {
-
 
     @Autowired
     private MessageConfig messageConfig;
@@ -25,30 +30,59 @@ public class msgSendController {
 
     @Autowired
     private MsgLogBiz msgLogBiz;
+    @Autowired
+    private MobileDataBiz mobileDataBiz;
 
-    @RequestMapping(value = "/sendPost")
+    @Autowired
+    private MobileChannelBiz mobileChannelBiz;
+
+    @RequestMapping(value = "/send")
     @ResponseBody
-    public ResponseEntity<?> sendPost(String mobile,String content) throws Exception {
+    public ResponseEntity<?> sendPost(@RequestParam String mobile,@RequestParam String content,@RequestParam String token) throws Exception {
 
-        //httpPost 发送
-        //return ResponseEntity.status(200).body(apiPathBiz.selectListAll());
-        Message msgLog = new Message();
-        msgLog.setMobile(mobile);
-        msgLog.setContent(content);
-        msgLog.setCreatetime(new Date());//加入队列时间
-        msgLogBiz.addMsgLog(msgLog);
-        redisQueueBiz.lpush(messageConfig.getQueueIn(), JSON.toJSONString(msgLog));
+        //检查 手机号码的有效性
+        if(mobile.indexOf(",")!= -1){
+            String[] moblies = mobile.split(",");
+            for(int i =0 ;i<moblies.length;i++){
+                if(!PhoneFormatCheckUtils.isChinaPhoneLegal(moblies[i])){
+                    return ResponseEntity.badRequest().body("{error:'invalid mobile'}");
+                }
+            }
+        }else{
+            if(!PhoneFormatCheckUtils.isChinaPhoneLegal(mobile)){
+                return ResponseEntity.badRequest().body("{error:'invalid mobile'}");
+            }
+        }
 
+        //通过手机号获取渠道号并写入相对应的队列
+         //队列统一命名 sms_企业代码
+        int mobileMiddle = Integer.parseInt(mobile.substring(0,7));
+        MobileData mobileData = mobileDataBiz.selectMobileData(mobileMiddle);
+
+        //根据 网段找到 相应的通道 信息 发送信息 (亦可以跟进号段 进行分配 渠道商的 号码段)
+        if(mobileData == null){
+            return ResponseEntity.badRequest().body("{error:'can not find send channel'}");
+        }
+        Message message = new Message();
+        message.setMobile(mobile);
+        message.setContent(content);
+        message.setCreatetime(new Date());//加入队列时间
+        //使用区号 作为队列
+        msgLogBiz.addMsgLog(message);
+
+        String channelQueue = ServerConstant.QUEUE_PREFIX+mobileData.getAreacode();
+
+        if(!redisQueueBiz.isSetValue(ServerConstant.WAITING_SET,channelQueue)){
+            redisQueueBiz.sSet(ServerConstant.WAITING_SET,channelQueue);
+        }
+        //System.out.println(channelQueue);
+        redisQueueBiz.lpush(channelQueue,JSON.toJSONString(message));
         return ResponseEntity.ok(200);
-
     }
 
     @RequestMapping(value = "/sendGet")
     @ResponseBody
     public ResponseEntity<?> sendGet(String mobile,String content) throws Exception {
-
-
         return  null;
-
     }
 }
